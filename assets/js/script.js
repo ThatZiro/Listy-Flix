6a3812a5569a8e6291e44eb3e5aea98a5c9abf07
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-10-06 18:54:09 -0400

    Added fetch to move api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-const omdbKey = eee9ecb3;
+const omdbKey = "git ";
 
 let input = "deadpool";
+
+$(document).ready(function () {
+  console.log("Document Ready!");
+
+  let url = `http://www.omdbapi.com/?apikey=${omdbKey}&t=${input}`;
+  let ourOptions = {
+    method: "GET",
+    cache: "reload",
+  };
+  GetApiJson(url, ourOptions).then((jsonData) => {
+    console.log(jsonData);
+  });
+});
